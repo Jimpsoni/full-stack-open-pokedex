c4dfa4f30d6684d42bf1d1538a6d260b48923a52
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-11-16 04:08:57 +0200

    Update pokedex.cy.js

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,8 +1,13 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    console.log("Visiting website")
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('We can navigate', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
   })
 })
